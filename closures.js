00ad2c4d9b58236b50dcf87a2feac7ea5838a47c
trabnett <timrabnett@gmail.com>
2019-01-10 23:55:19 +0000

    clousures activity countdown function with commented out loaded dice Lighthouse Labs w1d4

--- closures.js
+++ closures.js
@@ -1,23 +1,49 @@
-function makeLoadedDie() {
-  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
-  var num = -1
-  var roll = list[num]
+// function makeLoadedDie() {
+//   var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+//   var num = -1
+//   var roll = list[num]
 
-  return function() {
-  num += 1
-  return list[num]
+//   return function() {
+//   num += 1
+//   return list[num]
+//   }
+// }
+
+// var rollLoadedDie = makeLoadedDie();
+
+// console.log(rollLoadedDie());  // 5
+// console.log(rollLoadedDie());  // 4
+// console.log(rollLoadedDie());  // 6
+// console.log(rollLoadedDie());
+// console.log(rollLoadedDie());
+// console.log(rollLoadedDie());
+// console.log(rollLoadedDie());
+// console.log(rollLoadedDie());
+// console.log(rollLoadedDie());
+// console.log(rollLoadedDie());
+
+var countdownGenerator = function (x) {
+  var num = x
+  return function(){
+    if (num > 0) {
+      console.log("T-minus " + num)
+      num -= 1
+    }
+    else if (num === 0) {
+      console.log("Blast Off!")
+      num -= 1
+    }
+    else {
+      console.log("Rockets already gone, bub!")
+    }
+
   }
-}
+};
 
-var rollLoadedDie = makeLoadedDie();
-
-console.log(rollLoadedDie());  // 5
-console.log(rollLoadedDie());  // 4
-console.log(rollLoadedDie());  // 6
-console.log(rollLoadedDie());
-console.log(rollLoadedDie());
-console.log(rollLoadedDie());
-console.log(rollLoadedDie());
-console.log(rollLoadedDie());
-console.log(rollLoadedDie());
-console.log(rollLoadedDie());
+var countdown = countdownGenerator(3);
+countdown(); // T-minus 3...
+countdown(); // T-minus 2...
+countdown(); // T-minus 1...
+countdown(); // Blast Off!
+countdown(); // Rockets already gone, bub!
+countdown(); // Rockets already gone, bub!
